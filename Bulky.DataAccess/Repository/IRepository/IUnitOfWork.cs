01900c1be5a38e832158d71bf85983cdc06db652
Arsh Mansuri <m.arsh.mansuri@gmail.com>
2025-01-25 16:26:46 +0000

    Company CRUD implemented(126)

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,11 +1,12 @@
-﻿using BulkyBook.DataAccess.Repository.IRepository;
-
-namespace BulkyBook.DataAccess.Repository.IRepository
+﻿namespace BulkyBook.DataAccess.Repository.IRepository
 {
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
-        IProductRepository Product { get; } 
+        IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
+
+
 
         void Save();
     }
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,6 +1,4 @@
-﻿using BulkyBook.DataAccess.Repository.IRepository;
-using BulkyBook.DataAccess.Data;
-using BulkyBook.DataAccess.Repository;
+﻿using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository.IRepository;
 
 namespace BulkyBook.DataAccess.Repository
@@ -10,11 +8,13 @@ namespace BulkyBook.DataAccess.Repository
         private ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
+        public ICompanyRepository Company { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
         }
 
 
