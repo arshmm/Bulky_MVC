463ba695bc36dd62eb28e76aaf601b67f45eb853
Arsh Mansuri <m.arsh.mansuri@gmail.com>
2025-01-26 17:22:23 +0000

    SHopping cart model added and ApplicationUser repository created

---
+++ Bulky.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        void update(ShoppingCart obj);
+
+
+    }
+}
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository.IRepository;
 
+
 namespace BulkyBook.DataAccess.Repository
 {
     public class UnitOfWork : IUnitOfWork
@@ -9,12 +10,16 @@ namespace BulkyBook.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
         }
 
 
