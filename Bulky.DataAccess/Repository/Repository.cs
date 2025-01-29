cacd07827d294d00971b60c57da7c15666eb0f07
Arsh Mansuri <m.arsh.mansuri@gmail.com>
2025-01-29 19:15:03 +0000

    Shopping cart Order total and view model implemented(140)

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -49,9 +49,13 @@ namespace BulkyBook.DataAccess.Repository
 
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
             if (!string.IsNullOrEmpty(includeProperties))
             {
                 foreach (var includeProp in includeProperties
---
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyBook.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+
+        public double OrderTotal { get; set; }
+
+
+    }
+}
