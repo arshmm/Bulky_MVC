d327f3210b436dc2271ce4e1008ac793d74ab2aa
Arsh Mansuri <m.arsh.mansuri@gmail.com>
2025-02-12 13:42:59 +0000

    Ready for deployment

--- Bulky.Utilities/EmailSender.cs
+++ Bulky.Utilities/EmailSender.cs
@@ -1,26 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.Extensions.Configuration;
-using SendGrid;
-using SendGrid.Helpers.Mail;
 
 namespace BulkyBook.Utilities
 {
     public class EmailSender : IEmailSender
     {
-        public string SendGridSecret { get; set; }
+        //public string SendGridSecret { get; set; }
 
-        public EmailSender(IConfiguration _congif)
-        {
-            SendGridSecret = _congif.GetValue<string>("SendGrid:APIkey");
-        }
+        //public EmailSender(IConfiguration _congif)
+        //{
+        //    SendGridSecret = _congif.GetValue<string>("SendGrid:APIkey");
+        //}
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var client = new SendGridClient(SendGridSecret);
-            var from = new EmailAddress("m.arsh.mansuri@gmail.com", "Bulky Boook");
-            var to = new EmailAddress(email);
-            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            //var client = new SendGridClient(SendGridSecret);
+            //var from = new EmailAddress("m.arsh.mansuri@gmail.com", "Bulky Boook");
+            //var to = new EmailAddress(email);
+            //var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
 
-            return client.SendEmailAsync(message);
+            //return client.SendEmailAsync(message);
+            return Task.CompletedTask;
         }
     }
 }
